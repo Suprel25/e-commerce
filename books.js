3fcdd89bb5f516ad0fe282de2623e00172159d0f
Suprel Simmons <suprelsimmons@gmail.com>
2026-09-22 10:27:04 -0400

    completed

--- books.js
+++ books.js
@@ -1,11 +1,81 @@
+let books
 
+async function renderBooks (filter) {
+  const booksWrapper = document.querySelector('.books');
+
+  booksWrapper.classList += ' books__loading'
+
+  if (!books) {
+    books = await getBooks();
+  }
+
+  booksWrapper.classList.remove('books__loading')
+  
+if (filter === 'LOW_TO_HIGH') {   
+  books.sort((a,b) =>(a.salePrice||a.originalPrice) - (b.salePrice||b.originalPrice))
+}
+else if (filter === 'HIGH_TO_LOW'){
+  books.sort((a,b) =>(b.salePrice||b.originalPrice) - (a.salePrice||a.originalPrice))
+}
+else if (filter === 'RATING') {
+  books.sort((a,b) =>(b.rating) - (a.rating))
+}
+  
+
+  const booksHTML = books.map((book) => {
+    return `<div class="book">
+    <figure class="book__img--wrapper">
+      <img class="book__img" src="${book.url}" alt="">
+    </figure>
+    <div class="book__title">
+      ${book.title}
+    </div>
+    <div class="book__ratings">
+      ${ratingsHTML(book.rating)}
+    </div>
+    <div class="book__price">
+       ${priceHTML(book.originalPrice, book.salePrice)}
+    </div>
+  </div>`
+  })
+  .join('')
+
+
+  booksWrapper.innerHTML = booksHTML 
+}
+
+function priceHTML (originalPrice, salePrice){
+  if (!salePrice) {
+    return `$${originalPrice.toFixed(2)}`
+  }
+   return  `<span class="book__price--normal">$${originalPrice.toFixed(2)}</span> $${salePrice.toFixed(2)}` 
+}
+
+function ratingsHTML (rating) {
+  let ratingHTML = ''
+  for (let i = 0; i < Math.floor(rating); ++i ) {
+    ratingHTML += '<i class="fas fa-star"></i>\n'
+  }
+  if(!Number.isInteger(rating)){
+     ratingHTML += '<i class="fas fa-star-half-alt"></i>\n'
+  }
+  return ratingHTML
+}
+
+function filterBooks(event){
+  renderBooks(event.target.value)
+  }
+
+setTimeout(() => {
+  renderBooks ();
+});
 
 // FAKE DATA
 function getBooks() {
-  return new Promise((resolve) => {
+  return new Promise ((resolve,) => {
     setTimeout(() => {
       resolve([
-        {
+    {
           id: 1,
           title: "Crack the Coding Interview",
           url: "assets/crack the coding interview.png",
@@ -93,7 +163,7 @@ function getBooks() {
           salePrice: null,
           rating: 4.5,
         },
-      ]);
+      ])
     }, 1000);
-  });
+  })
 }
